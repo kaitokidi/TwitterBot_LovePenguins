c0bc93e173de72b97e50ffbfa97ffabc53450b7b
Genís Bayona <genis.bayona@est.fib.upc.edu>
2016-01-24 02:12:21 +0100

    This is it

--- myBot.py
+++ myBot.py
@@ -32,6 +32,12 @@ def dump(queue, info):
         json.dump(info, fi)
     
 def main():
+
+    #think is ok
+    r = random ()%4
+    if ( r > 2 ):
+        exit ()
+
     twitter = auth()
     queue, info = load()
     tweets = twitter.search(q="penguins", result_type="recent", since_id=info["sinceid"], count='100')
@@ -40,18 +46,6 @@ def main():
     to_add = [tweet for tweet in tweets["statuses"] if not tweet["retweeted"] and not tweet.has_key("retweeted_status")]
 
     #to_add = [tweet for tweet in to_add if (tweet["text"].__contains__(triggers[0]) or tweet["text"].__contains__(triggers[1]) or tweet["text"].__contains__(triggers[2]) or tweet["text"].__contains__(triggers[3]))]
-
-    #TOTEST
-
-    #totaltext = tweet["text"]
-    #print totaltext
-
-    #tt = [x for x in totaltext.split() if x[0] != '@']
-    #print tt
-    #t = ' '.join(tt)
-    #print t
-    #    [y for y in x if y != 2]
-    #to_add = [tweet for tweet in to_add if ( (triggers[0] in t) or (triggers[1] in t) or (triggers[2] in t) or (triggers[3] in t) )]
 
     filtered = []
     for tweet in to_add:
@@ -65,7 +59,6 @@ def main():
 	
         if ( (triggers[0] in t) or (triggers[1] in t) or (triggers[2] in t) or (triggers[3] in t) ):
             filtered.append(tweet)
-    #TOTEST
 
 
     queue = list(queue) + filtered

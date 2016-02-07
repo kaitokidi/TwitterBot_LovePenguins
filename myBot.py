d035ce4dd4421f8a006e019dfa966beaac25b22d
Genís Bayona <genis.bayona@est.fib.upc.edu>
2016-02-07 17:43:37 +0100

    Update myBot.py

--- myBot.py
+++ myBot.py
@@ -43,7 +43,7 @@ def main():
     tweets = twitter.search(q="penguins", result_type="recent", since_id=info["sinceid"], count='100')
     info["sinceid"] = tweets["search_metadata"]["max_id"]
     triggers = ("Penguins","penguins","Penguin","penguin")
-    forbiden = ("Hockey","hockey","match","winners","win","lost","vs","loosers","Pittsburgh","pittsburgh","pitsburg","Pitsburg","Winners")
+    forbiden = ("Hockey","hockey","match","winners","win","lost","vs","loosers","Pittsburgh","pittsburgh","pitsburg","Pitsburg","Winners","NHL","#NHL","nhl","victory","Crosby")
     to_add = [tweet for tweet in tweets["statuses"] if not tweet["retweeted"] and not tweet.has_key("retweeted_status")]
 
     #to_add = [tweet for tweet in to_add if (tweet["text"].__contains__(triggers[0]) or tweet["text"].__contains__(triggers[1]) or tweet["text"].__contains__(triggers[2]) or tweet["text"].__contains__(triggers[3]))]
@@ -63,7 +63,7 @@ def main():
 	notForbiden = True;
 	for word in forbiden:
             if(word in t):
-            notForbiden = False
+                notForbiden = False
         triggered = ((triggers[0] in t) or (triggers[1] in t) or (triggers[2] in t) or (triggers[3] in t))
         if (notForbiden and triggered ):
             filtered.append(tweet)
